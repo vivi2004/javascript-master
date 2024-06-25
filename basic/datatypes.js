e868cc813a88577b40162049c81d0ebf6b88d5c4
VIVEK KUMAR PURBEY <121883435+vivi2004@users.noreply.github.com>
2024-06-25 20:16:29 +0000

    data

---
+++ basic/datatypes.js
@@ -0,0 +1,17 @@
+" use strict "  // treat all js code as newer version.
+// alert ( 3+4) we are using node js , not browser
+ 
+//   code readability is must be priorites
+
+let name = " viie"
+let age = 20
+isPoor = true 
+ let state;
+  /*
+symbol
+bignit
+boolean
+null
+ object
+  */
+ console.log(typeof age)
