5fffcf1adad8ba73e77165db6cfa47d8103e1357
VIVEK KUMAR PURBEY <121883435+vivi2004@users.noreply.github.com>
2024-06-26 22:10:39 +0000

    date

---
+++ basic/numsMaths.js
@@ -0,0 +1,12 @@
+const balance = new Number( 1000)
+console.log(balance)
+const score = 400.99
+console.log(score)
+console.log(balance.toFixed(2))
+console.log(score.toPrecision(3))
+console.log(balance.toLocaleString('en-IN'))
+console.log((Math.random()*10) +1)
+console.log(Math.floor(Math.random()*10) +1)
+  const max = 30
+  const min = 20
+  console.log( Math.floor(Math.random()*(max -min +1) )+ min ) //  randomly value that is generated between  max and min value...
---
+++ basic/string.js
@@ -0,0 +1,21 @@
+// const name = " vivie"
+// const myId = " vivi231"
+// console.log( ` My name is  ${name}  and my Id is :${myId} `)
+
+const myName = new String("viviPurbey")
+console.log(myName[0])
+// console.log(myName.toUpperCase)
+console.log(myName.charAt(2))  // 2 is the index that will gave me the character of that index
+console.log( myName.indexOf('i'))
+ const newString = myName.slice(-1,3)
+ const newSubstring = myName.substring(2,4)
+ console.log(newSubstring)
+ const newStringOne = "     vivi    "
+ console.log(newStringOne)
+ console.log(newStringOne.trim()) // it erase all the wide space of the string .
+  const url ="https:// vivideveloper.com /vivi@20purbey"
+   console.log(url.replace("@20","_"))
+    console .log(url.includes('vivi'))
+    console.log(url.split("/"))
+    console.log(myName.small())
+    console.log(myName.valueOf())
